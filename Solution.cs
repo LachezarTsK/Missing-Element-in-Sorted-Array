ade0439fe38f614b471d4eaeaf79f95e8145ac9c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-01-18 15:02:42 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,32 @@
+
+using System;
+
+public class Solution
+{
+    public int MissingElement(int[] input, int target)
+    {
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left <= right)
+        {
+            int middlePoint = left + (right - left) / 2;
+
+            if (target > missingElementsTillCurrentIndex(input, middlePoint))
+            {
+                left = middlePoint + 1;
+            }
+            else
+            {
+                right = middlePoint - 1;
+            }
+        }
+        return input[0] + right + target;
+    }
+
+    private int missingElementsTillCurrentIndex(int[] input, int index)
+    {
+        int expectedNumberOfPreceedingElements = input[index] - input[0];
+        return expectedNumberOfPreceedingElements - index;
+    }
+}
